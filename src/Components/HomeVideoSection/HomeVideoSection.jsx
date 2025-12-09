045b18315540225a5e95a7da1a585e5978646296
askidea <askinnovativelab@gmail.com>
2025-12-09 23:29:34 +0530

    Video Commits

---
+++ src/Components/HomeVideoSection/HomeVideoSection.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './HomeVideoSection.css'
+import { Play } from 'lucide-react'
+
+const HomeVideoSection = () => {
+  return (
+    <div className="container" style={{marginBottom: '130px'}}>
+        <h1 className="video-section-heading">See How It Works</h1>
+        <div className="filters-section my-4">
+            <button className='filter-button active'>Postpartum Recovery Essential Kit </button>
+            <button className='filter-button'>C-Section Recovery Kit </button>
+            <button className='filter-button'>Mega Recovery Kit </button>
+        </div>
+        <div className="video-container">
+            <div className="video"><Play className='icon' /></div>
+        </div>
+    </div>
+  )
+}
+
+export default HomeVideoSection
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,10 +3,12 @@ import MomsHub from '../../Components/MomsHub/MomsHub'
 import ActivitiesHome from '../../Components/ActivitiesHome/ActivitiesHome'
 import FreeGuide from '../../Components/FreeGuide/FreeGuide'
 import StoriesHome from '../../Components/StoriesHome/StoriesHome'
+import HomeVideoSection from '../../Components/HomeVideoSection/HomeVideoSection'
 
 const Home = () => {
   return (
     <>
+      <HomeVideoSection />
       <StoriesHome />
       <ActivitiesHome />
       <FreeGuide />
