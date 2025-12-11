b7f83e5ee3ee43040dad6f1b3490d0041a6344c4
askidea <askinnovativelab@gmail.com>
2025-12-11 12:27:27 +0530

    Commits

--- src/Components/MommyFirstTrust/MommyFirstTrust.jsx
+++ src/Components/MommyFirstTrust/MommyFirstTrust.jsx
@@ -3,6 +3,8 @@ import Heading from '../Heading/Heading'
 import './MommyFirstTrust.css'
 import Loom from '../../assets/trust/loom.svg'
 import Trust22 from '../../assets/trust/trust22.svg'
+import Trust11 from '../../assets/trust/trust11.svg'
+import Trust111 from '../../assets/trust/trust11-1.svg'
 
 const MommyFirstTrust = () => {
     const headingData = {
@@ -16,7 +18,13 @@ const MommyFirstTrust = () => {
         <Heading data={headingData} />
         <div className="mommy-first-trust-container">
             <div className="first-grid">
-                <div className="tile-one"></div>
+                <div className="tile-one">
+                    <img src={Trust11} alt="" className='image' />
+                    <h1 className="heading">Cleanse with Care</h1>
+                    <p className="count"><span>9</span>/10</p>
+                    <p className="description">new moms say it’s an essential postpartum item</p>
+                    <img src={Trust111} alt="" />
+                </div>
                 <div className="tile-two"></div>
             </div>
 
