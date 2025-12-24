5f766ffb4a1602dac9efeb5878eaf91b33db63b1
askidea <askinnovativelab@gmail.com>
2025-12-24 07:26:45 +0530

    Minicart comits

--- src/Pages/Modals/MiniCartModal/MiniCartModal.jsx
+++ src/Pages/Modals/MiniCartModal/MiniCartModal.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import './MiniCartModal.css'
+import { X } from 'lucide-react'
 
 const MiniCartModal = () => {
   return (
     <div class="offcanvas offcanvas-end" tabindex="-1" id="MiniCartModal" aria-labelledby="offcanvasRightLabel">
-        <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="offcanvasRightLabel">Offcanvas right</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        <div className="mf-off-canvas-header">
+            <p className="head-ing">Cart <span className="cart-items-count">03</span></p>
+            <button className="close-btn" data-bs-dismiss="offcanvas" aria-label="Close"><X /></button>
         </div>
-        <div class="offcanvas-body">
-            ...
+        <div className="mini-cart-modal-body">
+          Items
+        </div>
+        <div className="minicart-modal-footer">
+          <p className="price-container">
+            <span>Sub total</span>
+            <span className="price">$199.99 USD</span>
+          </p>
+          <div className="button-container">
+            <button className='button-pink-center' style={{width: '48%', height: '40px', boxShadow: 'none'}}>Check out</button>
+            <button className='button-pink-border' style={{width: '48%', height: '40px', boxShadow: 'none'}}>View cart</button>
+          </div>
         </div>
     </div>
   )
