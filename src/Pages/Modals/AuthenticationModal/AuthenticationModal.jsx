1525f098fee46a7c64141489df4294db0e12ee12
askidea <askinnovativelab@gmail.com>
2025-12-24 20:05:24 +0530

    Commits

--- src/Pages/Modals/AuthenticationModal/AuthenticationModal.jsx
+++ src/Pages/Modals/AuthenticationModal/AuthenticationModal.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './AuthenticationModal.css'
 import { X } from 'lucide-react'
 import Google from '../../../assets/Google.svg'
@@ -6,6 +6,7 @@ import Facebook from '../../../assets/Facebook.svg'
 import Apple from '../../../assets/Apple.svg'
 
 const AuthenticationModal = () => {
+  const [toggleAction, setToggleAction] = useState('left')
   return (
     <div class="offcanvas offcanvas-end" tabindex="-1" id="AuthenticationModal" aria-labelledby="offcanvasRightLabel">
         <div className="mf-off-canvas-header">
@@ -13,7 +14,19 @@ const AuthenticationModal = () => {
             <button className="close-btn" data-bs-dismiss="offcanvas" aria-label="Close"><X /></button>
         </div>
         <div className="login-modal-body-con">
+          <div className="tab-switching-container">
+            <div className={`toggle-slider ${toggleAction == 'right' ? 'right' : ''} ${toggleAction == 'left' ? 'left' : ''}`}></div>
+            <button onClick={()=>setToggleAction('left')}>Login with Email</button>
+            <button onClick={()=>setToggleAction('right')}>Login with Mobile</button>
+          </div>
 
+          <div className="email-login-main-container">
+            <div className="mf-input-container">
+              <label>Enter your email address</label>
+              <input type="text" placeholder='example@email.com' />
+            </div>
+            <button className='button-pink-center' style={{boxShadow: 'none', marginTop: '22px'}} >Continue</button>
+          </div>
         </div>
         <div className="login-modal-footer">
           <div className="login-alternate-button-section">
