186de86d3b860fdda994fdaf2535b0870e63f19a
askidea <askinnovativelab@gmail.com>
2025-12-24 09:53:12 +0530

    Login Modal COmmits

--- src/Pages/Modals/AuthenticationModal/AuthenticationModal.jsx
+++ src/Pages/Modals/AuthenticationModal/AuthenticationModal.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import './AuthenticationModal.css'
+import { X } from 'lucide-react'
+import Google from '../../../assets/Google.svg'
+import Facebook from '../../../assets/Facebook.svg'
+import Apple from '../../../assets/Apple.svg'
 
 const AuthenticationModal = () => {
   return (
     <div class="offcanvas offcanvas-end" tabindex="-1" id="AuthenticationModal" aria-labelledby="offcanvasRightLabel">
-        <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="offcanvasRightLabel">Offcanvas right</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        <div className="mf-off-canvas-header">
+            <p className="head-ing">Login</p>
+            <button className="close-btn" data-bs-dismiss="offcanvas" aria-label="Close"><X /></button>
         </div>
-        <div class="offcanvas-body">
-            ...
+        <div className="login-modal-body-con">
+
+        </div>
+        <div className="login-modal-footer">
+          <div className="login-alternate-button-section">
+            <button><img src={Google} alt="" /> Sign in with Google</button>
+            <button><img src={Facebook} alt="" /> Sign in with Facebook</button>
+            <button><img src={Apple} alt="" /> Sign in with Apple</button>
+          </div>
         </div>
     </div>
   )
