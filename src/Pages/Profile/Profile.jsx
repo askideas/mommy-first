d85c8fbc9d5627aa5e6b71a893656f17bb8c8ccc
askidea <askinnovativelab@gmail.com>
2026-01-11 23:02:11 +0530

    Profile redirection commits

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,24 +1,146 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Profile.css'
 import ProfileImg from '../../assets/profile/pf-def.png'
 import { Info } from 'lucide-react'
+import UserIcon from '../../assets/profile/user-square.svg'
+import Smile from '../../assets/profile/smile.svg'
+import Map from '../../assets/profile/map.svg'
+import Card from '../../assets/profile/card.svg'
+import Box from '../../assets/profile/cube.svg'
+import Return from '../../assets/profile/return.svg'
+import Heart from '../../assets/profile/heart.svg'
+import Help from '../../assets/profile/help.svg'
+import Bell from '../../assets/profile/bell.svg'
+import Tag from '../../assets/profile/tag.svg'
+import Settings from '../../assets/profile/settings.svg'
+import { NavLink, useLocation } from 'react-router-dom'
 
 const Profile = () => {
-  return (
-    <div className="profile-main-container">
-        <div className="container">
-            <div className="profile-header-section">
-                <div className="profile-image">
-                    <img src={ProfileImg} alt="" />
-                    <span className="active"></span>
+    const location = useLocation();
+    const [activeSection, setActiveSection] = useState("#profile");
+
+    useEffect(() => {
+        const hash = location.hash;
+        setActiveSection(hash || "profile");
+    }, [location.hash]);
+
+    const Profilemenu = [
+        {
+            title: 'My Account',
+            items : [
+                {
+                    title : 'Profile',
+                    icon : UserIcon,
+                    hash : '#profile'
+                },
+                {
+                    title : 'My Babies',
+                    icon : Smile,
+                    hash : '#mybabies',
+                    label: 'OPTIONAL'
+                },
+                {
+                    title : 'Addresses',
+                    icon : Map,
+                    hash : '#address'
+                },
+                {
+                    title : 'Payments',
+                    icon : Card,
+                    hash : '#payments'
+                }
+            ]
+        },
+        {
+            title: 'Order related',
+            items : [
+                {
+                    title : 'My Orders',
+                    icon : Box,
+                    hash : '#orders'
+                },
+                {
+                    title : 'Returns & Exchanges',
+                    icon : Return,
+                    hash : '#returnandexchange'
+                },
+                {
+                    title : 'Wishlist',
+                    icon : Heart,
+                    hash : '#wishlist'
+                },
+                {
+                    title : 'Help',
+                    icon : Help,
+                    hash : '#help'
+                }
+            ]
+        },
+        {
+            title: false,
+            items : [
+                {
+                    title : 'Notifications',
+                    icon : Bell,
+                    hash : '#notifications'
+                },
+                {
+                    title : 'Subscriptions',
+                    icon : Tag,
+                    hash : '#subscriptions'
+                },
+                {
+                    title : 'Settings',
+                    icon : Settings,
+                    hash : '#settings'
+                }
+            ]
+        },
+    ]
+
+    return (
+        <div className="profile-main-container">
+            <div className="container">
+                <div className="profile-header-section">
+                    <div className="profile-image">
+                        <img src={ProfileImg} alt="" />
+                        <span className="active"></span>
+                    </div>
+                    <p className="profile-name">Hi, <strong>Sarah</strong> 👋 </p>
+                    <p className="greeting">Hope you’re feeling good today</p>
+                    <p className="info"><Info />You’re 20% there! Complete your profile to enjoy full benefits and special surprises ✨</p>
                 </div>
-                <p className="profile-name">Hi, <strong>Sarah</strong> 👋 </p>
-                <p className="greeting">Hope you’re feeling good today</p>
-                <p className="info"><Info />You’re 20% there! Complete your profile to enjoy full benefits and special surprises ✨</p>
+            </div>
+
+            <div className="container">
+                <div className="profile-details-main-container">
+                    <div className="profile-sidebar-con">
+                        {
+                            Profilemenu.map((item, index)=> {
+                                return (
+                                    <div className='profile-items-con'>
+                                        <h1 className="profile-menu-sec-heading">{item.title}</h1>
+                                        {
+                                            item.items.map((profileItem, index)=> {
+                                                return (
+                                                    <button className={`profile-menu-item ${activeSection === profileItem.hash ? "active" : ""}`} onClick={() => (window.location.hash = profileItem.hash.replace("#",""))} ><img src={profileItem.icon} alt="" />{profileItem.title}{profileItem.label && <span className='item-label'>{profileItem.label}</span>}</button>
+                                                )
+                                            })
+                                        }
+                                    </div>
+                                )
+                            })
+                        }
+                        <button className="logout-btn">Log Out</button>
+                    </div>
+
+                    <div className="profile-content-based-on-selection">
+                        
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Profile
