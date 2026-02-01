41871b67955538a526ab3ffa29511e78e67869df
askidea <askinnovativelab@gmail.com>
2026-02-01 15:46:54 +0530

    bundle recommended hero section

--- src/Pages/BundlesRecommended/BundlesRecommended.jsx
+++ src/Pages/BundlesRecommended/BundlesRecommended.jsx
@@ -2,17 +2,14 @@ import { Check, CircleCheck, Clock, SquareX, X } from 'lucide-react'
 import React, { useEffect, useState } from 'react'
 import './BundlesRecommended.css'
 import BoxImg from '../../assets/BundleRecom/box-img.png'
-import Flakes1 from '../../assets/BundleRecom/flakes1.svg'
-import Flakes2 from '../../assets/BundleRecom/flakes2.svg'
-import Flakes3 from '../../assets/BundleRecom/flakes3.svg'
-import Flakes4 from '../../assets/BundleRecom/flakes4.svg'
 import LabelImg from '../../assets/BundleRecom/label.png'
-import BundleTileImg from '../../assets/BundleRecom/bundle-tile-img.png'
+import BundleTileImg from '../../assets/BundleRecom/bundle-item-1.png'
 import MomsReviewsSlider from '../../Components/MomsReviewsSlider/MomsReviewsSlider'
 import BoughtTogether from '../../Components/BoughtTogether/BoughtTogether'
 import FaqSlider from '../../Components/FaqSlider/FaqSlider'
 import MomsMomentsSlider from '../../Components/MomsMomentsSlider/MomsMomentsSlider'
 import AllBundlesSlider from '../../Components/AllBundlesSlider/AllBundlesSlider'
+import HightLightImg from '../../assets/BundlesHome/badge.png'
 
 const BundlesRecommended = () => {
     const [movement, setMovement] = useState('')
@@ -25,76 +22,86 @@ const BundlesRecommended = () => {
   return (
     <>
         <div className="container">
-            <div className="bundles-recommended-heading">
-                <p className='head-ing'>Your recommended bundle, <br /> just for you!</p>
-                <div className="labels-con">
-                    <p className="label-item">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#5ED34B" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
-                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
-                        </svg>
-                        <span>Fast Shipping</span>
-                    </p>
-
-                    <p className="label-item">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#5ED34B" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
-                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
-                        </svg>
-                        <span>FREE Returns</span>
-                    </p>
-                </div>
-            </div>
-        </div>
-
-        <div className="container">
-            <div className={`bundle-recommented-item-details-container `}>
-                {/* <div className={`flakes-con ${movement}`}>
-                    <img src={Flakes1} alt="" className="flakes1" />
-                    <img src={Flakes2} alt="" className="flakes2" />
-                    <img src={Flakes3} alt="" className="flakes3" />
-                    <img src={Flakes4} alt="" className="flakes4" />
-                </div> */}
-                <div className={`bundle-recommented-item-details ${movement}`}>
-                    <div className="label-sec-con">
-                        <img src={LabelImg} alt="" />
-                        <p>Recommended</p>
-                    </div>
-                    <div className="d-flex flex-column h-100 justify-content-between align-items-center">
-                        <div className="headinf-sec-container">
-                            <p className="label">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clock-fill" viewBox="0 0 16 16">
-                                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z"/>
-                                </svg>
-                                5-7  Days of Care</p>
-                            <p className="head-txt">The First Week <br /> Healing System</p>
-                            <div className="recom-labels">
-                                <p className="label-item">24 Pad Liners</p>
-                                <p className="label-item">8 Underwear</p>
-                                <p className="label-item">1 Peri bottle</p>
-                                <p className="label-item">8 Cooling Pads</p>
-                                <p className="label-item">Witch Hazel Perineal Care Foam</p>
-                            </div>
-                            <div className="price-section-con">
-                                <div className="price-sec">$49.99</div>
-                                <div className="price-desc">Only $7.78/day — less than your daily coffee</div>
-                            </div>
-                        </div>
-
-                        <div className="bundles-recom-tile-footer">
-                            <img src={BundleTileImg} alt="" />
-                            <div className="check-container">
-                                <Check className='checkbox-icon' />
-                                <span>C-Section compatible</span>
-                            </div>
-                            <div className="d-flex justify-content-between align-items-center w-100" style={{marginTop: '14px'}}>
-                                <button className='button-pink-center' style={{width: '143px', height: '36px'}}>Buy Now</button>
-                                <button className='button-pink-border' style={{width: '143px', height: '36px'}}>Add to Bag +</button>
-                            </div>
-                        </div>
-                    </div>
-                    
+            <div className="bundles-recommended-container-hero-section">
+                <div className="heading-section-bundles">
+                    <h1>Your recommended bundle,just for you!</h1>
+                    <h2>Take a breath. We’re always here to help you find the bundle that fits your needs.</h2>
+                    <button className='button-pink-border'>CONTINUE SHOPING</button>
                 </div>
 
-                <div className={`bundle-recommented-item-details-optional ${movement} `}>
+                <div className="bundles-best-value-section">
+                    <div className="image-highlist">
+                        <img src={HightLightImg} alt="" />
+                        <span>Best Value</span>
+                    </div>
+
+                    <p className="heading-label-sec">
+                        <span className="bundle-name">2 Weeks Full Recovery Set</span>
+                        <span className="days-label">7-14 days</span>
+                    </p>
+
+                    <p className="bundle-description">Best for core support for the hardest days at home</p>
+
+                    <img src={BundleTileImg} alt="" className="bundle-tile-image" />
+
+                    <div className="bundle-items">
+                        <span>36 Pads</span>
+                        <span>20 Underwear</span>
+                        <span>20 Cooling pads</span>
+                        <span>48 Liners</span>
+                        <span>10 fl oz foam</span>
+                        <span>Peri bottle + Bag</span>
+                    </div>
+
+                    <svg className="line-separator" width="295" height="1" viewBox="0 0 295 1" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <line y1="0.5" x2="294.667" y2="0.5" stroke="#F2B8C6"/>
+                        <line y1="0.5" x2="294.667" y2="0.5" stroke="url(#paint0_linear_14742_6974)"/>
+                        <defs>
+                        <linearGradient id="paint0_linear_14742_6974" x1="0" y1="1.5" x2="294.667" y2="1.5" gradientUnits="userSpaceOnUse">
+                        <stop stop-color="white"/>
+                        <stop offset="0.490385" stop-color="#EAA4B9"/>
+                        <stop offset="0.971154" stop-color="white"/>
+                        </linearGradient>
+                        </defs>
+                    </svg>
+
+                    <p className="bundle-price">
+                        <span className="price">$139.99</span>
+                        <span className="price-label">Retail value $195 | Save $55.01</span>
+                    </p>
+
+                    <button className="button-pink-center">ADD TO BAG</button>
+
+                    <div className="feature-of-bundle">
+                        <p>
+                            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M16.6666 5L7.49992 14.1667L3.33325 10" stroke="#5ED34B" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                            <span>Secure checkout</span>
+                        </p>
+                        <p>
+                            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M16.6666 5L7.49992 14.1667L3.33325 10" stroke="#5ED34B" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                            <span>Loved by moms</span>
+                        </p>
+                        <p>
+                            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M16.6666 5L7.49992 14.1667L3.33325 10" stroke="#5ED34B" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                            <span>FREE shipping</span>
+                        </p>
+                        <p>
+                            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M16.6666 5L7.49992 14.1667L3.33325 10" stroke="#5ED34B" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                            <span>Hassle free returns</span>
+                        </p>
+                    </div>
+
+                </div>
+
+                <div className="optional-bundles-to-items-container">
                     <div className="close-container">
                         <div className="label-div">Optional</div>
                         <SquareX className='close-btn' onClick={()=> setMovement('')} />
