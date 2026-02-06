be6e00e47bd46c5710e3974c1b6dcdf34160b7a1
askidea <askinnovativelab@gmail.com>
2026-02-06 19:59:22 +0530

    c

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -17,8 +17,9 @@ const About = () => {
             <div className="about-page-heading-container">
                 <p className="heading">Who we are!</p>
                 <img src={Flower} alt="" className='flower' />
-                <p className="sub-heading">The Science of Safety. <br />
-                    <span className='sub-heading-span'>The Heart of a <span className='strike-span'>Mother.</span> </span>
+                <p className="sub-heading">
+                    <span className='from-left'>The Science of Safety.</span> <br />
+                    <span className='sub-heading-span from-right'>The Heart of a <span className='strike-span'>Mother.</span> </span>
                 </p>
                 <p className="description">Born in New York from the partnership of a Clinical Pharmacist who saw the <br /> medical gap, and a Clinical Quality Specialist who lived through it.</p>
                 
