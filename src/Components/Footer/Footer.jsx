db1f1adf8208471cf9d9718183b4105393cd8d08
askidea <askinnovativelab@gmail.com>
2026-02-06 16:34:15 +0530

    fc

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -22,37 +22,37 @@ const Footer = () => {
                 <div className="menu-items-section">
                     <div className="menu-item-section">
                         <p className='menu-heading'>About</p>
-                        <NavLink to="/about" target='_blank'>Our Story</NavLink>
-                        <NavLink to="/stories" target='_blank'>Reviews</NavLink>
-                        <NavLink to="/events" target='_blank'>Events</NavLink>
-                        <NavLink to="/blogs" target='_blank'>News Room</NavLink>
-                        <NavLink to="/donation" target='_blank'>Giving Back</NavLink>
+                        <NavLink to="/about" >Our Story</NavLink>
+                        <NavLink to="/stories" >Reviews</NavLink>
+                        <NavLink to="/events" >Events</NavLink>
+                        <NavLink to="/blogs" >News Room</NavLink>
+                        <NavLink to="/donation" >Giving Back</NavLink>
                     </div>
 
                     <div className="menu-item-section">
                         <p className='menu-heading'>Shop</p>
-                        <NavLink to="/shop" target='_blank'>Shop All</NavLink>
-                        <NavLink tp="/bundles" target='_blank'>Exclusive Bundles <span className='flash-animation' style={{background: '#FF1F1F', color: '#ffffff'}}>Sale</span></NavLink>
-                        <NavLink to="/collection/pregnancy-care" target='_blank'>Pregnancy Care</NavLink>
-                        <NavLink to="/collection/postpartum-care" target='_blank'>Postpartum Care</NavLink>
-                        <NavLink to="/collection/breast-care" target='_blank'>Breast Care</NavLink>
-                        <NavLink to="/collection/baby-care" target='_blank'>Baby Care</NavLink>
+                        <NavLink to="/shop" >Shop All</NavLink>
+                        <NavLink tp="/bundles" >Exclusive Bundles <span className='flash-animation' style={{background: '#FF1F1F', color: '#ffffff'}}>Sale</span></NavLink>
+                        <NavLink to="/collection/pregnancy-care" >Pregnancy Care</NavLink>
+                        <NavLink to="/collection/postpartum-care" >Postpartum Care</NavLink>
+                        <NavLink to="/collection/breast-care" >Breast Care</NavLink>
+                        <NavLink to="/collection/baby-care" >Baby Care</NavLink>
                     </div>
 
                     <div className="menu-item-section">
                         <p className='menu-heading'>Community</p>
-                        <NavLink to="/care-hub" target='_blank'>Care Hub <span className='flash-animation' style={{background: '#5ED34B', color: '#ffffff'}}>New</span></NavLink>
+                        <NavLink to="/care-hub" >Care Hub <span className='flash-animation' style={{background: '#5ED34B', color: '#ffffff'}}>New</span></NavLink>
                         <NavLink>Care Guides</NavLink>
                         <NavLink>Live Sessions</NavLink>
-                        <NavLink to="/blogs" target='_blank'>Blog</NavLink>
-                        <NavLink to="/events" target='_blank'>Events</NavLink>
+                        <NavLink to="/blogs" >Blog</NavLink>
+                        <NavLink to="/events" >Events</NavLink>
                         <NavLink>Education</NavLink>
-                        <NavLink to="/donation" target='_blank'>Giving Back</NavLink>
+                        <NavLink to="/donation" >Giving Back</NavLink>
                     </div>
 
                     <div className="menu-item-section">
                         <p className='menu-heading'>Help</p>
-                        <NavLink to="/faqs" target='_blank'>FAQs</NavLink>
+                        <NavLink to="/faqs" >FAQs</NavLink>
                         <NavLink>Order Tracking</NavLink>
                         <NavLink to="/returns-refunds">Returns & Reunds</NavLink>
                         <NavLink to="/contact">Contact Us</NavLink>
